cea655f5ac25d33ea4a9e60bb655020821405e8b
DionisisSchoinas <p17134@students.cs.unipi.gr>
2020-10-31 23:41:38 +0200

    Added Firebolt and Explosion

---
+++ Assets/Scripts/Spells/Firebolt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firebolt : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 2f;
+    [SerializeField]
+    private GameObject explosion;
+    [SerializeField]
+    private Rigidbody rb;
+
+    void FixedUpdate()
+    {
+        rb.AddForce(transform.forward * speed * Time.deltaTime, ForceMode.Impulse);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Destroy(Instantiate(explosion, transform.position + transform.forward * 0.2f, transform.rotation), 0.3f);
+        Destroy(gameObject);
+    }
+}
